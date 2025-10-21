6a45767703c12062c7b3eb3901e01762b6a5daa4
nasimxonov <saidnurmuhammadulloxonn@gmail.com>
2025-10-21 20:28:09 +0500

    main.ts updated

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const logger = new Logger('Bootstrap');
+  const PORT = process.env.PORT ?? 3000;
+
+  await app.listen(PORT);
+
+  logger.log(`Server is running on port ${PORT}`);
 }
 bootstrap();
