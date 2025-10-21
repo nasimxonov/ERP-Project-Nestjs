6e71ca8d90b82f099ca6765104a0ca58f3b9f886
nasimxonov <saidnurmuhammadulloxonn@gmail.com>
2025-10-21 20:26:55 +0500

    database module added

---
+++ src/core/database/prisma.service.ts
@@ -0,0 +1,30 @@
+import {
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+  OnModuleDestroy,
+  OnModuleInit,
+} from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  private readonly logger = new Logger(PrismaService.name);
+
+  async onModuleInit() {
+    try {
+      await this.$connect();
+      this.logger.log('Database connected');
+    } catch (error) {
+      throw new InternalServerErrorException(error);
+    }
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+    this.logger.error('Database disconnect');
+  }
+}
