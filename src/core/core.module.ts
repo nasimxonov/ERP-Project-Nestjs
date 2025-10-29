f14b7fd2a814ede80236eb20cc33df33c4b85398
nasimxonov <saidnurmuhammadulloxonn@gmail.com>
2025-10-29 17:22:03 +0500

    Jwt added

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
 import { SeedModule } from './database/seed/seed.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 
 Module({
-  imports: [DatabaseModule, SeedModule],
+  imports: [
+    DatabaseModule,
+    SeedModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      global: true,
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get('JWT_KEY'),
+        signOptions: {
+          expiresIn: '2h',
+        },
+      }),
+    }),
+  ],
   exports: [DatabaseModule],
 });
 export class CoreModule {}
