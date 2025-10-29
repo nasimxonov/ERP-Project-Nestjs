3a30603f2a63507b6a72f6fc0fb448fdb38d35d2
nasimxonov <saidnurmuhammadulloxonn@gmail.com>
2025-10-29 10:29:52 +0500

    interceptor added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
 import { CoreModule } from './core/core.module';
+import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
+import TransformInterceptor from './common/interceptors/transform.interceptor';
+import { AuthGuard } from './common/guard/auth.guard';
 
 @Module({
   imports: [CoreModule],
-  providers: [],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: TransformInterceptor,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+  ],
 })
 export class AppModule {}
