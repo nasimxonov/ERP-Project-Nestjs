771b36991f23398d3b4713d380f0b22077154b65
nasimxonov <saidnurmuhammadulloxonn@gmail.com>
2025-10-30 14:08:06 +0500

    edited

--- src/core/database/seed/seed.service.ts
+++ src/core/database/seed/seed.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PrismaService } from '../prisma.service';
-import  bcrypt from 'bcrypt';
+import bcrypt from 'bcrypt';
 import { Role } from '@prisma/client';
 
 @Injectable()
@@ -18,11 +18,11 @@ export class SeedService implements OnModuleInit {
   async seedUsers() {
     this.logger.log('Users seeders started');
 
-    const first_name = this.configService.get('SUPERADMIN_FIRSTNAME');
-    const last_name = this.configService.get('SUPERADMIN_LASTNAME');
-    const email = this.configService.get('SUPERADMIN_EMAIL');
-    const username = this.configService.get('SUPERADMIN_USERNAME');
-    const password = this.configService.get('SUPERADMIN_PASSWORD');
+    const first_name = this.configService.get('SUPERADMIN_FIRSTNAME') as string;
+    const last_name = this.configService.get('SUPERADMIN_LASTNAME') as string;
+    const email = this.configService.get('SUPERADMIN_EMAIL') as string;
+    const username = this.configService.get('SUPERADMIN_USERNAME') as string;
+    const password = this.configService.get('SUPERADMIN_PASSWORD') as string;
 
     const findExistsAdmin = await this.prisma.user.findFirst({
       where: { username },
