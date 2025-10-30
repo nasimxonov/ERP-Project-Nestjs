56d19aca6103800259d110ba792fa8a3bb2678a0
nasimxonov <saidnurmuhammadulloxonn@gmail.com>
2025-10-30 14:09:25 +0500

    cors added

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,51 @@
 import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import cookieParser from 'cookie-parser';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  app.setGlobalPrefix('api');
+  app.use(cookieParser());
+  app.enableCors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('ERP Project API')
+    .setDescription(
+      'Autentifikatsiya va foydalanuvchi boshqaruvi uchun API hujjatlari',
+    )
+    .setVersion('1.0')
+    .addBearerAuth()
+    .addCookieAuth('token')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document, {
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+  });
 
   const logger = new Logger('Bootstrap');
   const PORT = process.env.PORT ?? 3000;
 
   await app.listen(PORT);
-
   logger.log(`Server is running on port ${PORT}`);
+  logger.log(`Swagger docs: http://localhost:${PORT}/api/docs`);
 }
 bootstrap();
